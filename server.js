f13e1e0f595742b2e0c8fbda21b1128f5d697d9a
Ahmed Elsherbeeny <a.elrwesh@gmail.com>
2021-04-27 12:40:11 +0200

    the weather journal

--- server.js
+++ server.js
@@ -1,23 +1,55 @@
 // Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
+projectData = [];
 
+// Express to run server and routes
+const express = require('express');
+const path = require('path');
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
+var bodyParser = require('body-parser')
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+var cors = require('cors');
+
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
 // Spin up the server
+const port = 3000;
+
+const server = app.listen(port, listener);
+
 // Callback to debug
+function listener() {
+    console.log(`Server is running and listening on port : ${port}`);
+}
 
 // Initialize all route with a callback function
-
+app.get('/all', sendData);
 // Callback function to complete GET '/all'
-
+function sendData(req, res) {
+    console.log(projectData);
+    res.send(projectData);
+    projectData = [];
+}
 // Post Route
-
+app.post('/add', (req, res) => {
+    console.log(req.body);
+    newData= {
+        city: req.body.city,
+        date: req.body.date,
+        temperature: req.body.temperature,
+        content: req.body.content
+    }
+    projectData.push(newData);
+});
 
---
+++ website/app.js
@@ -0,0 +1,77 @@
+// Global Variables
+
+const zip = document.getElementById('formGridCode_zip');
+const feeling = document.getElementById('formGridCode_feelings');
+const generate = document.getElementById('generate');
+const form = document.getElementById("form");
+
+
+// Create New Date 
+let d = new Date();
+let newDate = d.getMonth()+'-'+d.getDate()+'-'+d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const baseURL = "http://api.openweathermap.org/data/2.5/forecast?zip=" //524901&appid={API key}
+const apiKey = "&appid=9e1bdc7671c097b6ba1d69c1e4061a91&units=metric";
+
+// Event listener to add function to existing HTML DOM element
+form.addEventListener('submit', getData)
+/* Function called by event listener */
+function getData(e){ 
+    let newZip = zip.value;
+    console.log(newZip);
+    let newFeeling = feeling.value;
+    console.log(newFeeling);
+    getWeather(baseURL, newZip, apiKey)
+        .then(function(data) {
+            console.log(data);
+            postData('/add', {city: data.city.name, date: newDate, temperature: data.list[0].main.temp, content: newFeeling});
+            updateUi();
+        })
+    e.preventDefault();
+}
+/* Function to GET Web API Data*/
+const getWeather = async (baseURL, zip, key) => {
+    const result = await fetch(baseURL+zip+key)
+        try {
+            const data = await result.json();
+            return data
+        }catch(error) {
+            console.log("error", error);
+        }
+    
+}
+/* Function to POST data */
+ const postData = async (url = '', data = {})=> {
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }); 
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }catch(error) {
+        console.log("Error", error);
+    }
+}
+
+/* Function to GET Project Data */
+
+const  updateUi = async () => {
+    const getProjectData = await fetch('/all');
+    try {
+        const allData = await getProjectData.json();
+        document.getElementById('city').innerHTML =  `🏙️ ${allData[0].city} 🏙️`;
+        document.getElementById('date').innerHTML =  ` ${allData[0].date} `;
+        document.getElementById('temp').innerHTML =  ` ${allData[0].temperature} °C `;
+        document.getElementById('content').innerHTML =  ` ${allData[0].content} `;
+    }catch (error) {
+        console.log('Error :', error);
+    }
+}
